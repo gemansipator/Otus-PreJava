9349e39746bd236b851b3036a59ac61b9d665a7c
gemansipator <gemansipator@gmail.com>
2024-03-21 13:42:10 +0300

    01 Язык Java Примеры с byte закончеты

--- src/javalanguage/syntax/Main.java
+++ src/javalanguage/syntax/Main.java
@@ -3,12 +3,23 @@ package javalanguage.syntax;
 public class Main {
     public static void main(String[] args) {
         //System.out.println("Hello world II!");
+        //byte (1 байт), short (2), int (4), long  (8)
+        byte a = 1;
+        printBinary(a, 8);  //длину (8 бит в одном байте) ставим мы
+        a = 64;   //максимум 127
+        printBinary(a, 8);
+        a = 127;   //максимум 127
+        printBinary(a, 8);
+        a = -1;   //минимум  -128
+        printBinary(a, 8);
+        a = -128;   //минимум  -128
+        printBinary(a, 8);
     }
-    private static void printBinary(byte value, int size){
+    private static void printBinary(byte value, int size){ //берется байт, и размер В БИТАХ,
         for (int i = size -1; i >= 0; i--){
             long mask = 1 << i;
             long result = (mask & value) >> i;
-            System.out.println(result);
+            System.out.print(result); //печатает в консоль значение бита с лева направо
         }
         System.out.println();
 
