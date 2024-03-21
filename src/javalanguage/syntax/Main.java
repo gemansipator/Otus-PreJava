2ba469bea4b95fac31911916c5276ac71cea6e6e
gemansipator <gemansipator@gmail.com>
2024-03-21 14:58:45 +0300

    Примеры с int

--- src/javalanguage/syntax/Main.java
+++ src/javalanguage/syntax/Main.java
@@ -3,19 +3,24 @@ package javalanguage.syntax;
 public class Main {
     public static void main(String[] args) {
         //System.out.println("Hello world II!");
-        //byte (1 байт), short (2), int (4), long  (8)
-        byte a = 1;
-        printBinary(a, 8);  //длину (8 бит в одном байте) ставим мы
-        a = 64;   //максимум 127
-        printBinary(a, 8);
-        a = 127;   //максимум 127
-        printBinary(a, 8);
-        a = -1;   //минимум  -128
-        printBinary(a, 8);
-        a = -128;   //минимум  -128
-        printBinary(a, 8);
+        //byte (1 байт или 8 бит), short (2 байта), int (4 байта или 32 бита), long  (8 байт)
+
+        //NT_MIN	Минимальное значение для переменной типа int.	-2147483647 - 1
+        //INT_MAX	Максимальное значение для переменной типа int.	2147483647
+        int a = 0;
+        System.out.println("a = " + a);
+        printBinary(a, 32);
+        a = 22;
+        System.out.println("a = " + a);
+        printBinary(a, 32);
+        a = 2147483647;     // можно число написать так: 2_147_483_647
+        System.out.println("a = " + a);
+        printBinary(a, 32);
+        a = Integer.MAX_VALUE;    //максимальное число в Int
+        System.out.println("a = " + a);
+        printBinary(a, 32);
     }
-    private static void printBinary(byte value, int size){ //берется количество байт, и размер В БИТАХ,
+    private static void printBinary(int value, int size){ //берется количество байт, и размер В БИТАХ,
         for (int i = size -1; i >= 0; i--){
             long mask = 1 << i;
             long result = (mask & value) >> i;
