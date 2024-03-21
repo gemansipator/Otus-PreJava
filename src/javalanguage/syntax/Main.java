83d57aa983ae9a0230939212151f08166f9a7b34
gemansipator <gemansipator@gmail.com>
2024-03-21 15:17:08 +0300

    Примеры с Long

--- src/javalanguage/syntax/Main.java
+++ src/javalanguage/syntax/Main.java
@@ -5,24 +5,15 @@ public class Main {
         //System.out.println("Hello world II!");
         //byte (1 байт или 8 бит), short (2 байта), int (4 байта или 32 бита), long  (8 байт)
 
-        //NT_MIN	Минимальное значение для переменной типа int.	-2147483647 - 1
-        //INT_MAX	Максимальное значение для переменной типа int.	2147483647
-        int a = 0;
+
+        long a = Long.MAX_VALUE;
         System.out.println("a = " + a);
-        printBinary(a, 32);
-        a = 22;
-        System.out.println("a = " + a);
-        printBinary(a, 32);
-        a = 2147483647;     // можно число написать так: 2_147_483_647
-        System.out.println("a = " + a);
-        printBinary(a, 32);
-        a = Integer.MAX_VALUE;    //максимальное число в Int
-        System.out.println("a = " + a);
-        printBinary(a, 32);
+        printBinary(a, 64);
+
     }
-    private static void printBinary(int value, int size){ //берется количество байт, и размер В БИТАХ,
+    private static void printBinary(long value, int size){ //берется количество байт, и размер В БИТАХ,
         for (int i = size -1; i >= 0; i--){
-            long mask = 1 << i;
+            long mask = 1L << i;               //в случае long не забываем поставить L
             long result = (mask & value) >> i;
             System.out.print(result); //печатает в консоль значение бита с лева направо
         }
